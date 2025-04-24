663b4d917e2b110275b04bd4ffb4ea41b2f1a848
bhushan21z <v-bhushan.wanjari@elitmus.com>
2025-04-24 12:18:47 +0530

    seperate out code

--- app.js
+++ app.js
@@ -10,9 +10,4 @@ app.get('/home', (req, res) => {
 
 app.use('/users', userRoutes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
 module.exports = app;
